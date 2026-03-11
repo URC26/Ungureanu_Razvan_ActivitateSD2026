e4196d57ac75f37e105f38a53f59e8c0cfe77b22
RazvanCos <urc26@yahoo.com>
2026-03-11 23:14:04 +0200

    Laborator 1 - Lucru cu functii si structura Telefon

--- Project1/Seminar01.c
+++ Project1/Seminar01.c
@@ -1,8 +1,8 @@
-#include<stdio.h>
-int main() {
-	printf("Salutare!\nIntroduceti un numar intreg:");
-	int variabila = 0;
-	scanf_s("%d", &variabila);
-	printf("Ai introdus: %d", variabila);
-	return 0;
-}
+//#include<stdio.h>
+//int main() {
+//	printf("Salutare!\nIntroduceti un numar intreg:");
+//	int variabila = 0;
+//	scanf_s("%d", &variabila);
+//	printf("Ai introdus: %d", variabila);
+//	return 0;
+//}
